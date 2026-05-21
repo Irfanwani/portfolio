9b36e05bffc4037d171ec186b28c61560e1bfa95
Irfanwani <irfanwani347@gmail.com>
2026-05-21 20:20:14 +0530

    use dynamic year

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -1,9 +1,10 @@
 const Footer = () => {
+  let year = new Date().getFullYear();
   return (
     <section className="section section-footer reveal">
       <footer>
         <p className="footer-heading">Thank You for Visiting</p>
-        <p className="footer-sub">End of transmission · Irfan Wani · 2025</p>
+        <p className="footer-sub">End of transmission · Irfan Wani · {year}</p>
       </footer>
     </section>
   );
